3e7af4c331f24d8e683c713de60dd8e496def8b0
Marvin <MarvinCurme@gmail.com>
2019-12-24 11:43:57 -0500

    init files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace students_manage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
